5d09404075639011eed5e8830ae0e7c18a615510
Garvit Arora <garvit.university@gmail.com>
2025-07-18 19:17:06 +0530

    adding routers

---
+++ reactRouters/src/main.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Router, RouterProvider } from 'react-router-dom'
+import Root from './Root.jsx'
+import About from './components/About/About.jsx'
+import Home from './components/Home/Home.jsx'
+import Contact from './Contact/Contact.jsx'
+import { Route } from 'react-router-dom'
+import User from './components/User/User.jsx'
+
+// const router = createBrowserRouter([
+//   {
+//     path : '/',
+//     element : <Root />,
+//     children : [
+//       {
+//         path : "",
+//         element : <Home />
+//       },
+//       {
+//         path : "about",
+//         element : <About />
+//       },
+//       {
+//         path : "contact",
+//         element : <Contact />
+//       }
+//     ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path='/' element = {<Root />} >
+    <Route path='' element={<Home />} />
+    <Route path='about' element={<About />} />
+    <Route path='contact' element={<Contact />} />
+    <Route path='user/:userId' element={<User />} />
+
+  </Route>
+))
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router ={router} />
+  </StrictMode>,
+)
