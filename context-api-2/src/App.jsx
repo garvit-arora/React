d4852e0866c14eeb2d4970c2c4903683da9348bd
Garvit Arora <garvit.university@gmail.com>
2025-07-22 23:48:51 +0530

    practicing context-api

---
+++ context-api-2/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1>Context API 2</h1>
+    </>
+  )
+}
+
+export default App
