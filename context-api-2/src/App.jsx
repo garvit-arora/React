dec32b1805b19ca09164e1d8159c77097fbdea41
Garvit Arora <garvit.university@gmail.com>
2025-07-25 23:04:25 +0530

    practicing context-api

--- context-api-2/src/App.jsx
+++ context-api-2/src/App.jsx
@@ -19,13 +19,12 @@ function App() {
     
   }
   return (
-    <>
+  
     <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
     <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
             <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
               <ThemeBtn />
-                
             </div>
             <div className="w-full max-w-sm mx-auto">
                <Card />
@@ -33,7 +32,7 @@ function App() {
         </div>
     </div>
     </ThemeProvider>
-    </>
+  
   )
 }
 
