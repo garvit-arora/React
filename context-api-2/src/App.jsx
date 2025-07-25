f039c38fb06c8ea60170d7ad7a78dc21b6c25727
Garvit Arora <garvit.university@gmail.com>
2025-07-25 17:01:44 +0530

    practicing context-api

--- context-api-2/src/App.jsx
+++ context-api-2/src/App.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './contexts/themes'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 function App() {
-
-
+  const [themeMode,setThemeMode] = useState("light")
+  const lightTheme = ()=>{
+    setThemeMode("light");
+  }
+  useEffect(()=>{
+      document.querySelector('html').classList.remove("light","dark");
+      document.querySelector('html').classList.add(themeMode)
+    },[themeMode])
+  const darkTheme = ()=>{
+    setThemeMode("dark")
+    
+  }
   return (
     <>
-    <h1>Context API 2</h1>
-    <h2>Learning new things in React !!</h2>
+    <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
+    <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+              <ThemeBtn />
+                
+            </div>
+            <div className="w-full max-w-sm mx-auto">
+               <Card />
+            </div>
+        </div>
+    </div>
+    </ThemeProvider>
     </>
   )
 }
