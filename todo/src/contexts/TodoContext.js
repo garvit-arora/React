9961b91975d8b752848faeb9f84051f4ea6262b9
Garvit Arora <garvit.university@gmail.com>
2025-07-28 22:09:56 +0530

    practicing context-api

--- todo/src/contexts/TodoContext.js
+++ todo/src/contexts/TodoContext.js
@@ -1,10 +1,26 @@
 import { createContext, useContext } from "react";
 
 
-export const TodoContext = createContext({});
+export const TodoContext = createContext({
+    todos : [
+        {
+            id : 1,
+            todos : "Todo Msg",
+            completed : false,
+        }
+    ],
+    addTodo : (todo) =>{},
+    updateTodo : (id,todo) =>{},
+    deleteTodo : (id) =>{},
+    toggleComplete : (id) =>{}
+});
 
 
+export const UseTodo =()=>{
+    return useContext(TodoContext);
+}
 
+export const TodoProvider = TodoContext.Provider;
 
 
 
