5deea667f5bbef72134d8db3c74ec5922125e3a4
Garvit Arora <garvit.university@gmail.com>
2025-08-03 23:40:43 +0530

    make todo using redux

--- ReduxToolkitTodo/src/components/Todos.jsx
+++ ReduxToolkitTodo/src/components/Todos.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
-
+import { useDispatch, useSelector } from 'react-redux'
+import { removeTodo } from '../features/todo/todoSlice'
 function Todos() {
+    const todos = useSelector(state=>state.todos)
+    const dispatch = useDispatch()
     return (
         <>
     <div>Todos</div>
