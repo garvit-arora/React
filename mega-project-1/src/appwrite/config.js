34a4454a81cf481fd9f34619e24e681acbe3f2ff
Garvit Arora <garvit.university@gmail.com>
2025-08-11 06:09:52 +0530

    making mega-project

--- mega-project-1/src/appwrite/config.js
+++ mega-project-1/src/appwrite/config.js
@@ -74,6 +74,51 @@ export class Service{
             return false;
         }
     }
+    async getPosts(queries = [Query.equal("status","active")]){
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries,
+                100,
+                0,
+            )
+        } catch (error) {
+            console.log("Apprwrite service :: createPost :: error " , error);
+            return false;
+        }
+    }
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique,
+                file
+            )
+            return true;
+        } catch (error) {
+            console.log("Apprwrite service :: createPost :: error " , error);
+            return false;
+        }
+    }
+    async deleteFile(fileId){
+        try {
+            return await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true;
+        } catch (error) {
+            console.log("Apprwrite service :: createPost :: error " , error);
+            return false;
+        }
+    }
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+    }
 };
 
 const service = new Service();
