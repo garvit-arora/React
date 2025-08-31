2cb754a1af387fb8c118aa3013ce5f3ff1abb0e9
Garvit Arora <garvit.university@gmail.com>
2025-08-31 23:56:53 +0530

    make mega project

--- mega-project-1/src/App.jsx
+++ mega-project-1/src/App.jsx
@@ -1,39 +1,38 @@
-import {useDispatch} from 'react-redux'
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import './App.css'
-import authService from './appwrite/auth'
-import { logout } from "./store/authSlice"
-import {Header}   from "../src/components/index"
-import {Footer}   from "../src/components/index"
+import { useDispatch } from "react-redux";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import "./App.css";
+import authService from "./appwrite/auth";
+import { logout } from "./store/authSlice";
+import { Header } from "../src/components/index";
+import { Footer } from "../src/components/index";
 
 function App() {
-  const [loading,setLoading] = useState(true);
-  const dispatch = useDispatch()
-  useEffect(()=>{
-    authService.getUserAccount()
-    .then((userData)=>{
-      if (userData) {
-        dispatch(login({userData}))
-      } else {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getUserAccount()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
           dispatch(logout());
-      }
-    })
-    .finally(()=>setLoading(false))
-  },[])
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
   return !loading ? (
     <>
-    <div className='min-h-screen flex flex-wrap content-between bg-gray-400 '></div>
-    <div className="w-full block ">
-      <Header />
-      <main>
-      {/* <Outlet /> */}TODO:
-      </main>
-      <Footer />
-    </div>
-      </>
-  ) : null
+      <div className="min-h-screen flex flex-wrap content-between bg-gray-400 ">
+        <div className="w-full block ">
+          <Header />
+          <main className="text-center">{/* <Outlet /> */}TODO:</main>
+          <Footer />
+        </div>
+      </div>
+    </>
+  ) : null;
 }
 
-
-export default App
+export default App;
